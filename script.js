248c3f912afd269d1bf2dd9fd61317a4f9cf450e
Baxtiyor786 <jomoliddinovbaxtiyor@gmail.com>
2025-01-25 18:13:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// //Function Declaration 
+// function Xaditupi (x) {
+//     return x * x;
+// }
+// console.log(Xaditupi(24))
+
+
+// //Function Expression
+// const cube = function(x) {
+//     return x ** 3;
+// }
+// console.log(cube(2))
+
+// //  Function  
+// const multiply = (a,b) => a*b;
+// console.log(multiply(2,3));
+
+// // Параметры - это переменые который указываются при обьявлении функции. Они задают какие данные функции
+// function greet(name) {
+//     console.log(`Привет, ${name}!`);
+// }
+
+// //
+// greet("Абдулла") //Абдулла-это аргумент
+
+
+
+function lightMode(){
+    document.body.style.backgroundColor = '#C4B5A5';
+    document.body.style.color = 'black'
+}
+function darkMode(){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white'
+}
